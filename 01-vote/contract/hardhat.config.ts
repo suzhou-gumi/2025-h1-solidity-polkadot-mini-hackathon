108cb3bd6b57dfbeedddf5e94fa1e1f3f720c73c
gumi <suzhou.gumi@gmail.com>
2025-05-18 14:27:03 +0800

    feat: add polkadot

--- 01-vote/contract/hardhat.config.ts
+++ 01-vote/contract/hardhat.config.ts
@@ -1,8 +1,31 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@parity/hardhat-polkadot";
+import "@parity/hardhat-polkadot-resolc";
 
-const config: HardhatUserConfig = {
+require("dotenv").config();
+
+const config: any = {
   solidity: "0.8.28",
+
+  // npm Compiler
+  resolc: {
+    version: "1.5.2",
+    compilerSource: "npm",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    westendAssetHub: {
+      polkavm: true,
+      url: "https://westend-asset-hub-eth-rpc.polkadot.io",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
